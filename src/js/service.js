082584df5b9c4fd39f2e73d9a5f430f301b226b0
sherlock <920825209@qq.com>
2014-09-25 00:29:01 +0800

    修复一些ug

--- src/js/service.js
+++ src/js/service.js
@@ -1,155 +1,149 @@
 var MobileUI = {
 
-    bottomTrinagle : $(".bottom-trinagle"),
-    screen01       : $("#screen00"),
-    screenAll      : $("#screen-all"),
-    topTrinagle    : $(".top-trinagle"),
-    bgMain         : $("#bg-main"),
-    scaleDom       : $("#scaleDom"),
-    tranMain       : $("#nav-trinagle-main"),
-    leftContent    : $("#left-content")
+    bottomTrinagle: $(".bottom-trinagle"),
+    screen01: $("#screen00"),
+    screenAll: $("#screen-all"),
+    topTrinagle: $(".top-trinagle"),
+    bgMain: $("#bg-main"),
+    scaleDom: $("#scaleDom"),
+    tranMain: $("#nav-trinagle-main"),
+    leftContent: $("#left-content")
 };
 
 var MobileEvent = {
-	init : function(){
-		this.form();
+    init: function () {
+        this.form();
         //保证android下流畅 启动动画
-       MobileUI.screen01.removeClass("hide");
-	},
-	form : function(){
+        MobileUI.screen01.removeClass("hide");
+    },
+    form: function () {
 
-          //设置当前三角高度
-          var height  = document.body.offsetHeight;
-          var width   = document.body.offsetWidth;
+        //设置当前三角高度
+        var height = document.body.offsetHeight;
+        var width = document.body.offsetWidth;
 
-          var $leftZhe =   $("#paper-trin-left");
-          var $rightZhe =  $("#paper-trin-right");
+        var $leftZhe = $("#paper-trin-left");
+        var $rightZhe = $("#paper-trin-right");
 
 
-          $(".paper-trin").css({ "borderWidth": height/2+"px 0 "+height/2+"px "+width+"px"});
+        $(".paper-trin").css({ "borderWidth": height / 2 + "px 0 " + height / 2 + "px " + width + "px"});
 
-        $leftZhe.css("top","-"+height+"px");
-        $rightZhe.css("top",height+"px");
+        $leftZhe.css("top", "-" + height + "px");
+        $rightZhe.css("top", height + "px");
 
 
         //旋转的角度
-        var   transArray  = [30,90,150,210,270,330];
+        var transArray = [30, 90, 150, 210, 270, 330];
 
 
-          //滚动条
-		  var wrapper_scroll = new Scroller('#main', {
-                Scontainer : '.screen-all',
-                hScroll : true,
-                vScroll : false,
-                momentum : true,
-                bounce : false,
-                snap: true,
-                scrollBefore: function(name, e){
+        //滚动条
+        var wrapper_scroll = new Scroller('#main', {
+            Scontainer: '.screen-all',
+            hScroll: true,
+            vScroll: false,
+            momentum: true,
+            bounce: false,
+            snap: true,
+            scrollBefore: function (name, e) {
 
-                },
-                onScroll: function(name, obj){
-                },
-                onTouchEnd: function(name, obj){
-                },
-                scrollEnd: function(index){
+            },
+            onScroll: function (name, obj) {
+            },
+            onTouchEnd: function (name, obj) {
+            },
+            scrollEnd: function (index) {
 //                    console.log(index);
 
-                    var $screen = MobileUI.screenAll.find(".screen");
-                    var node = $screen.filter("[id='screen0"+index+"']");
-                    for (var i = 0; i < $screen.length; i++) {
-                        var $sc = $($screen[i]);
-                        $sc.children(".content").addClass("hide");
-                    };
-                    node.children(".content").removeClass("hide");
+                var $screen = MobileUI.screenAll.find(".screen");
+                var node = $screen.filter("[id='screen0" + index + "']");
+                for (var i = 0; i < $screen.length; i++) {
+                    var $sc = $($screen[i]);
+                    $sc.children(".content").addClass("hide");
                 }
-            });
-            //第一阶段结束动画
-            util.animationEnd(document.querySelector(".center-title"),function(){
-                //第一阶段隐藏
-                hideFirstFrame();
-            });
-            function hideFirstFrame(){
-                var  f1 = MobileUI.screen01.find("#left-decor");
-                var  f2 = MobileUI.screen01.find("#right-decor");
-                var  f3 = MobileUI.screen01.find(".center-title");
-
-                setTimeout(function(){
-                    MobileUI.bottomTrinagle.find("#trinagle-list-04").removeClass("hide");
-                    MobileUI.bottomTrinagle.find("#trinagle-list-05").removeClass("hide");
-                    MobileUI.topTrinagle.removeClass("hide");
-                    MobileUI.screen01.addClass("f2");
-                },1000);
-
+                ;
+                node.children(".content").removeClass("hide");
             }
-            var topTrinagle =  MobileUI.topTrinagle[0].querySelector(".trinagle-pour");
-            //第二阶段结束
-            util.animationEnd(topTrinagle,function(){
+        });
+        //第一阶段结束动画
+        util.animationEnd(document.querySelector(".center-title"), function () {
+            //第一阶段隐藏
+            hideFirstFrame();
+        });
+        function hideFirstFrame() {
+            var f1 = MobileUI.screen01.find("#left-decor");
+            var f2 = MobileUI.screen01.find("#right-decor");
+            var f3 = MobileUI.screen01.find(".center-title");
+
+            setTimeout(function () {
+                MobileUI.bottomTrinagle.find("#trinagle-list-04").removeClass("hide");
+                MobileUI.bottomTrinagle.find("#trinagle-list-05").removeClass("hide");
+                MobileUI.topTrinagle.removeClass("hide");
+                MobileUI.screen01.addClass("f2");
+            }, 1000);
+
+        }
+
+        var topTrinagle = MobileUI.topTrinagle[0].querySelector(".trinagle-pour");
+        //第二阶段结束
+        util.animationEnd(topTrinagle, function () {
 //              console.log("top trin");
 //              //动画重置
-                MobileUI.bgMain.siblings().addClass("hide");
-                MobileUI.bgMain.removeClass("hide");
-            });
-            //第三阶段动画
-            util.animationEnd(document.querySelector("#bg-response"),function(){
-                $("#bg-response").addClass("hide");
-            });
-            //第四阶段动画
-            util.animationEnd(document.querySelector("#fourAniEnd"),function(){
-                $("#screen01").css("background","#ffffff");//
-                $("#scaleDom").addClass("hide");
+            MobileUI.bgMain.siblings().addClass("hide");
+            MobileUI.bgMain.removeClass("hide");
+        });
+        //第三阶段动画
+        util.animationEnd(document.querySelector("#bg-response"), function () {
+            $("#bg-response").addClass("hide");
+        });
+        //第四阶段动画
+        var fourAniEnd  =   document.querySelector("#fourAniEnd");
+        util.animationEnd(fourAniEnd,function () {
+            var $ma =   $("#nav-trinagle-main");
+            $("#screen01").css("background", "#ffffff");
+            MobileUI.scaleDom.addClass("scaleDom");
+            $("#paper-01").addClass("hide");
+            $(fourAniEnd).addClass("hide");
 
+            setTimeout(function(){
+                MobileUI.scaleDom.addClass("hide");
+                $ma.removeClass("hide");
+                MobileUI.leftContent.removeClass("hide");
+            },900);
 
-                $("#nav-trinagle-main").removeClass("hide");
-
-                $("#nav-trinagle-main").css({
-                    "-webkit-transform" : "rotateZ(30deg) scale(1.5)"
-                });
-
-
-            });
-
-            //计数器
-            var count = 0;
-            //旋转角度
-            MobileUI.tranMain.tap(function(event){
-                count++;
-                if(count > transArray.length){
-                    count = 0;
-                }
-
-                var targetDom  = MobileUI.leftContent.find(".left-content-ul").filter("[num='"+count+"']");
-                targetDom.siblings(".left-content-ul").addClass("hide");
-                targetDom.removeClass("hide");
-
-                MobileUI.tranMain.css({"-webkit-transform":"rotateZ("+transArray[count]+"deg) scale(1.5) translateZ(0)"});
-            });
+        });
 
 
 
-            function showLeftContent(num){
-                MobileUI.leftContent.find(".left-content-ul").filter("[num="+num+"]").removeClass("hide")
-                    .siblings().addClass("hide");
+        //计数器
+        var count = 0;
 
+        function showLeftContent(num) {
+            MobileUI.leftContent.find(".left-content-ul").filter("[num='" + num + "']").removeClass("hide")
+                .siblings().addClass("hide");
+
+        }
+
+
+        MobileUI.tranMain.find("i").tap(function () {
+
+            count++;
+            if (count > transArray.length-1) {
+                count = 0;
             }
 
-
-
-
-
-        MobileUI.tranMain.find("i").tap(function(){
             var num = $(this).attr("num");
             console.log(num);
             showLeftContent(num);
+            MobileUI.tranMain.css({"-webkit-transform": "rotateZ(" + transArray[count] + "deg) scale(1.5) translateZ(0)"});
+            return  false;
         });
 
 
         //跳转子页面链接
-        $("#left-content").on("tap","li>a",function(){
+        MobileUI.leftContent.on("tap", "li>a", function () {
             var href = $(this).attr("href");
             window.location.href = href;
         });
-
-
 
 
     }
@@ -158,7 +152,6 @@ var MobileEvent = {
 };
 
 
-
-$(function(){
-	MobileEvent.init();
+$(function () {
+    MobileEvent.init();
 });
